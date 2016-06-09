525510a0595721e0d6c201cd404b8c9a7132bedc
Augusto Roccasalva <augusto@rioplomo.com.ar>
2016-06-09 18:40:25 -0300

    Refactor application methods

--- app/components/employees-view.vue
+++ app/components/employees-view.vue
@@ -1,34 +1,28 @@
 <template>
+<p v-if="error">{{ error }}</p>
 <ul class="list">
   <li v-for="employee in objects">
     {{ employee.last_name }}, {{ employee.first_name }} ({{ employee.cuil }})
   </li>
 </ul>
-<p>Showing {{ objects.length }} of {{ num_results }}</p>
+<p v-if="objects">Showing {{ objects.length }} of {{ num_results }}</p>
 </template>
 
 <script>
 export default {
-  data () {
-    return { }
-  },
-  ready () {
-    this.$http.get('employees').then((response) => {
-      // get status
-      response.status
-
-      // get all headers
-      response.headers()
-
-      // get 'expires' header
-      response.headers('expires')
-
-      // set data on vm
-      // this.$set('employees', response.data['objects'])
-      this.$set('$data', response.data)
-    }, (response) => {
-      // error callback
-    })
+  data () { return {} },
+  ready () { this.loadEmployees() },
+  methods: {
+    loadEmployees () {
+      console.log('loadEmployees')
+      this.$http.get('employees').then((response) => {
+        // response is ok
+        this.$set('$data', response.data)
+      }, (response) => {
+        // handle error
+        console.log('error:', response)
+      })
+    }
   }
 }
 </script>
