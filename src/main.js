c4137c91c266420ea54e177e2cbfb399f0702233
Augusto Roccasalva <augusto@rioplomo.com.ar>
2016-06-09 09:10:27 -0300

    Remove unused vue-request header

--- src/main.js
+++ src/main.js
@@ -9,7 +9,6 @@ Vue.use(Router)
 // Install resource
 Vue.use(Resource)
 Vue.http.options.root = 'http://localhost:5000/api/v1'
-Vue.http.headers.common['Authorization'] = 'Basic abcd02394'
 
 Vue.config.debug = true
 
