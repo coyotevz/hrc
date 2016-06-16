addb4bfd220a8eec467d82ce7b3224cbe5614eaa
Augusto Roccasalva <augusto@rioplomo.com.ar>
2016-06-16 20:45:42 -0300

    Add vue-mdl and required style

--- app/components/application.vue
+++ app/components/application.vue
@@ -4,31 +4,32 @@
       <h1>Human Resource Application</h1>
       <p>An application to manage and track small people teams</p>
     </v-view>
+    <mdl-button v-mdl-ripple-effect>Ripple Effect</mdl-button>
   </div>
 </template>
 
 <style lang="scss">
-html {
-  height: 100%;
-}
-
-body {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-  margin: 0;
-}
-
-#application {
-  color: #2c3e50;
-  max-width: 600px;
-  font-family: Source Sans Pro, Helvetica, sans-serif;
-  text-align: center;
-
-  a {
-    color: #42b983;
-    text-decoration: none;
+  html {
+    height: 100%;
   }
-}
+
+  body {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    margin: 0;
+  }
+
+  #application {
+    color: #2c3e50;
+    max-width: 600px;
+    font-family: Source Sans Pro, Helvetica, sans-serif;
+    text-align: center;
+
+    a {
+      color: #42b983;
+      text-decoration: none;
+    }
+  }
 </style>
--- app/main.js
+++ app/main.js
@@ -1,10 +1,17 @@
 import Vue from 'vue'
 import { StateManager } from 'voie'
 import Resource from 'vue-resource'
+import VueMdl from 'vue-mdl'
+
+require('material-design-lite/material.css')
+require('material-design-lite/material.js')
 
 // Install resource
 Vue.use(Resource)
 Vue.http.options.root = 'http://localhost:5000/api/v1'
+
+// Install vue mdl
+Vue.use(VueMdl)
 
 Vue.config.debug = true
 
