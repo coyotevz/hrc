e3cdaac1ba886cc7d5bde85e8a1014d08fe4081f
Augusto Roccasalva <augusto@rioplomo.com.ar>
2016-06-19 05:13:18 -0300

    Fix width for employees layout

--- app/components/employees.vue
+++ app/components/employees.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="employees-layout mdl-grid mdl-cell--8-col">
-    <div v-for="employee in employees" class="mdl-cell mdl-cell--6-col">
+  <div class="employees-layout mdl-grid">
+    <div v-for="employee in employees" class="mdl-cell mdl-cell--4-col">
       <employee-card :employee="employee"></employee-card>
     </div>
   </div>
@@ -17,4 +17,7 @@ export default {
 </script>
 
 <style>
+  .employees-layout {
+    max-width: 1024px;
+  }
 </style>
